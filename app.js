97bc6dc30f6d9b029046760eb3a4f3dc6a098bc7
Kelton Person <kelton.person@gmail.com>
2013-02-28 19:51:01 -0500

    adjusted save method

--- app.js
+++ app.js
@@ -66,9 +66,10 @@ io.sockets.on('connection',function(socket) {
 	
 		
 	socket.on('createTrivia', function(data){
-		var name = data.triviaName;
-		var trivia = new Trivia({triviaName : name, sessionId : clientInfo[socket.id].sessionId });
-		trivia.save();
+		console.log(data);
+		//var name = data.triviaName;
+		//var trivia = new Trivia({triviaName : name, sessionId : clientInfo[socket.id].sessionId });
+		//trivia.save();
 	});
 	
 	socket.on('register', function(data){
