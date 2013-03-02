ef3a183390a49e9a92072f60c4f1be8b8c37223e
Kelton Person <kelton.person@gmail.com>
2013-03-02 18:43:32 -0500

    updeate random function and add utility class

--- assets/js/fragment.js
+++ assets/js/fragment.js
@@ -111,4 +111,20 @@ JS.require('JS.Observable', function() {
 		}
 	});
 	
+	Utility = new JS.Class({ 
+		
+		initialize: function(localBroadcaster, remoteBroadcaster) {	
+		},
+		
+		randomString : function(len) {
+		    var text = "";
+		    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+		    for( var i=0; i < len; i++ )
+		        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+		    return text;	
+		}
+	});
+	
 });
